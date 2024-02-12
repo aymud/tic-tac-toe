d60d0faa2c4bcd74986aecf88cb947a5212bb39f
Ahmed <aaa26770123@gmail.com>
2024-02-12 15:57:01 -0700

    Clean up code

--- src/main.rs
+++ src/main.rs
@@ -12,14 +12,14 @@ fn main() -> Result<(), slint::PlatformError> {
         move || {
             let ui = ui_handle.unwrap();
             let mut board: Vec<GamePieceData> = ui.get_board().iter().collect();
-            // let winner: Option<String> = check_winner(&mut board);
-            // println!("the winner is: {winner:?}")
-            if let Some((a, b, c)) = check_winner(&board) {
+            if let Some((a, b, c)) = get_indices_of_winning_combination(&board) {
                 // Update properties of the winning combination.
                 let green_color = Color::from_rgb_u8(0, 128, 0);
                 board[a].marker_color = green_color;
                 board[b].marker_color = green_color;
                 board[c].marker_color = green_color;
+
+                println!("the winner is: {}", get_winner_marker(&board, (a, b, c)));
 
                 let board_model = ModelRc::from(Rc::new(VecModel::from(board)));
                 ui.set_board(board_model);
@@ -29,7 +29,7 @@ fn main() -> Result<(), slint::PlatformError> {
     ui.run()
 }
 
-fn check_winner(grid: &Vec<GamePieceData>) -> Option<(usize, usize, usize)> {
+fn get_indices_of_winning_combination(grid: &Vec<GamePieceData>) -> Option<(usize, usize, usize)> {
     const WINNING_COMBINATIONS: &[(usize, usize, usize)] = &[
         (0, 1, 2), (3, 4, 5), (6, 7, 8), // Rows
         (0, 3, 6), (1, 4, 7), (2, 5, 8), // Columns
@@ -38,15 +38,15 @@ fn check_winner(grid: &Vec<GamePieceData>) -> Option<(usize, usize, usize)> {
 
     // Check each winning combination.
     for &(a, b, c) in WINNING_COMBINATIONS {
-        if let (ch1, ch2, ch3) = (
-            grid[a].marker.clone(),
-            grid[b].marker.clone(),
-            grid[c].marker.clone()) {
-            if !ch1.is_empty() && ch1 == ch2 && ch2 == ch3 {
-                return Some((a, b, c)); // winner winner chicken dinner.
-            }
+        if !grid[a].marker.is_empty()
+            && grid[a].marker == grid[b].marker
+            && grid[b].marker == grid[c].marker {
+            return Some((a, b, c)); // winner winner chicken dinner.
         }
     }
-
     None // No winner found.
+}
+
+fn get_winner_marker(board: &[GamePieceData], (a, _, _): (usize, usize, usize)) -> String {
+    return board[a].marker.clone().to_string();
 }
