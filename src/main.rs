10d09ad48e742c864508ee03885b0e8678abbace
Ahmed <aaa26770123@gmail.com>
2024-02-12 15:40:31 -0700

    Change color of winning combo to green

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use slint::Model;
+use std::rc::Rc;
+
+use slint::{Color, Model, ModelRc, VecModel};
 
 slint::include_modules!();
 
@@ -9,16 +11,25 @@ fn main() -> Result<(), slint::PlatformError> {
         let ui_handle = ui.as_weak();
         move || {
             let ui = ui_handle.unwrap();
-            let board: Vec<GamePieceData> = ui.get_board().iter().collect();
-            let winner: Option<String> = check_winner(&board);
-            println!("the winner is: {winner:?}")
+            let mut board: Vec<GamePieceData> = ui.get_board().iter().collect();
+            // let winner: Option<String> = check_winner(&mut board);
+            // println!("the winner is: {winner:?}")
+            if let Some((a, b, c)) = check_winner(&board) {
+                // Update properties of the winning combination.
+                let green_color = Color::from_rgb_u8(0, 128, 0);
+                board[a].marker_color = green_color;
+                board[b].marker_color = green_color;
+                board[c].marker_color = green_color;
+
+                let board_model = ModelRc::from(Rc::new(VecModel::from(board)));
+                ui.set_board(board_model);
+            }
         }
     });
-
     ui.run()
 }
 
-fn check_winner(grid: &Vec<GamePieceData>) -> Option<String> {
+fn check_winner(grid: &Vec<GamePieceData>) -> Option<(usize, usize, usize)> {
     const WINNING_COMBINATIONS: &[(usize, usize, usize)] = &[
         (0, 1, 2), (3, 4, 5), (6, 7, 8), // Rows
         (0, 3, 6), (1, 4, 7), (2, 5, 8), // Columns
@@ -32,7 +43,7 @@ fn check_winner(grid: &Vec<GamePieceData>) -> Option<String> {
             grid[b].marker.clone(),
             grid[c].marker.clone()) {
             if !ch1.is_empty() && ch1 == ch2 && ch2 == ch3 {
-                return Some(ch1.to_string()); // winner winner chicken dinner.
+                return Some((a, b, c)); // winner winner chicken dinner.
             }
         }
     }
